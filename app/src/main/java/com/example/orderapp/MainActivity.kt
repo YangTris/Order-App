d1081a27769afbb001a94097a5df5df8a7ed7f3d
Khánh Phạm Phú <phamphukhanh.sgu.edu@gmail.com>
2024-01-24 21:00:10 +0700

    Getting started with Kotlin syntaxes
    Trying to put the package name on activity_main.xml

--- app/src/main/java/com/example/orderapp/MainActivity.kt
+++ app/src/main/java/com/example/orderapp/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.example.orderapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var PACKAGE_NAME: String
-        PACKAGE_NAME = applicationContext.packageName
-        println("Hello World")
-        print("Package name: " + PACKAGE_NAME)
+        // Lấy tham chiếu đến TextView từ layout
+        val textViewPackageName: TextView = findViewById(R.id.textViewPackageName)
+
+        // Gán giá trị cho TextView
+        textViewPackageName.text = applicationContext.packageName
     }
 }
