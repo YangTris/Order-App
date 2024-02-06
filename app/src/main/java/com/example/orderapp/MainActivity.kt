ba40fed71c9457260e0316b09845a8f0c833eaf4
YangTris <id.vantri911@gmail.com>
2024-02-06 15:43:43 +0700

    Feat: add user data

--- app/src/main/java/com/example/orderapp/MainActivity.kt
+++ app/src/main/java/com/example/orderapp/MainActivity.kt
@@ -4,17 +4,18 @@ import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.example.orderapp.databinding.ActivityMainBinding
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
-
+import androidx.databinding.DataBindingUtil
+import com.example.orderapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val db = Firebase.firestore
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: User
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val db = Firebase.firestore
 
         // Create a new user with a first and last name
         val user = hashMapOf(
@@ -34,4 +35,26 @@ class MainActivity : AppCompatActivity() {
                 Log.w(TAG, "Error adding document", e)
             }
     }
+
+    /*private fun readUserData() {
+        // Assuming you have a collection named "users"
+        db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    //Log.d(TAG, "${document.id} => ${document.data}")
+                    val userName = document.getString("userName")
+                    val email = document.getString("email")
+                    val password = document.getString("password")
+                    val phoneNumber = document.getString("phoneNumber")
+
+                    // Set data to ViewModel
+                    val userModel = User("1",userName, email, password, phoneNumber,"1")
+                    binding.viewModel = userModel
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
+    }*/
 }
